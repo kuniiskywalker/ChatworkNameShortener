ee442b5ad08355c90d6b6609be711f3e3f33d351
kunii <kunii@cueplus.com>
2016-02-04 20:40:55 +0900

    [modify] チャット入力時に宛先を変換する処理を追加

--- scripts/content.js
+++ scripts/content.js
@@ -31,7 +31,7 @@ function handleMutatedNode(node) {
 
     var source = function (ids, script_tag_id) {
         ids.forEach(function (id) {
-            _chatworkNameShortener(id);
+            _CNS.updateTimeline(id);
         });
         document.body.removeChild(document.querySelector("script[data-id='" + script_tag_id + "']"));
     };
@@ -49,21 +49,89 @@ function handleMutatedNode(node) {
 
 document.body.appendChild(function () {
 
-        var sourceCodeOfMethod = function (id) {
+        // check exists message
+        var getMsgById = function (id) {
             var chat = RM.timeline.chat_id2chat_dat[id];
-            if (!chat || !chat.msg || !chat.msg.match(/^(\[To:\d+\]).*\r?\n/gm)) {
+            if (!chat || !chat.msg) {
                 return false;
             }
-            var msg = chat.msg.replace(/^(\[To:\d+\]).*\r?\n/gm, '$1').replace(/(\[.*\])/, '$1\n');
-            var message = document.querySelector("[data-mid='" + id + "'] pre");
-            if (message) {
-                message.innerHTML = CW.renderMessage(msg, {mid: id});
-            }
+            return chat.msg;
         };
 
+        // empty name
+        var getEmptyName = function (msg) {
+            
+            // check converted
+            var a = msg.split(/\r\n|\r|\n/)
+                .filter(function (b) {
+                    if (b) return b;
+                })
+                .filter(function (b) {
+                    var c = b.match(/(\[To:\d+\])/g);
+                    if (c && c.length > 1) {
+                        return a;
+                    }
+                });
+            if (a.length > 1) {
+                return false;
+            }
+            
+            // check more than 2 messages
+            var matches = msg.match(/^(\[To:\d+\]).*\r?\n?/gm);
+            if (!matches || matches.length <= 1) {
+                return false;
+            }
+
+            return msg.replace(/^(\[To:\d+\]).*\r?\n?/gm, '$1').replace(/(\[.*\])/, '$1\n');
+        };
+
+        // update timeline message
+        var updateTimeline = function (id) {
+
+            var msg = _CNS.getMsgById(id);
+            if (!msg) {
+                return false;
+            }
+            
+            var empty_name = _CNS.getEmptyName(msg);
+            if (!empty_name) {
+                return false;
+            }
+
+            var timelineElement = document.querySelector("[data-mid='" + id + "'] pre");
+            if (timelineElement) {
+                timelineElement.innerHTML = CW.renderMessage(empty_name, {mid: id});
+            }
+        };
+        
+        var chatTextConvertEvent = function () {
+            var chatText = document.querySelector('#_chatText');
+            chatText.addEventListener('blur', function () {
+                var msg = _CNS.getEmptyName(chatText.value);
+                if (!msg) {
+                    return false;
+                }
+                chatText.value = msg;
+            });
+            chatText.addEventListener('click', function () {
+                var msg = _CNS.getEmptyName(chatText.value);
+                if (!msg) {
+                    return false;
+                }
+                chatText.value = msg;
+            });
+        };
+        
+        var src = "var _CNS = {};\n";
+        src +="_CNS.getMsgById = " + getMsgById.toString() + ";\n";
+        src +="_CNS.getEmptyName = " + getEmptyName.toString() + ";\n";
+        src +="_CNS.updateTimeline = " + updateTimeline.toString() + ";\n";
+
+        src += "(" + chatTextConvertEvent.toString() + ") ()";
+        
         var sc = document.createElement("script");
         sc.type = "text/javascript";
-        sc.text = "var _chatworkNameShortener = "+sourceCodeOfMethod.toString();
+        sc.text = src;
         return sc
     } ()
 );
