07c29e467b632424501514aa0eda0635d426019a
國井弘一 <koichi_kunii@bita.jp>
2018-05-16 17:58:24 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 // @file App.js
+
+
 (function(){
     let Cns = require('./Cns').default;
 
-    let chatText = document.querySelector('#_chatText');
-    let update = function () {
-        let msg = chatText.value;
-        if (!msg || Cns.isDeleted(msg)) {
-            return false;
-        }
+    const wait = () => {
+        setTimeout(() => {
+            if (document.getElementById('_chatText')) {
+                inject();
+                return;
+            }
+            wait();
+        }, 0);
+    }
+    wait();
 
-        chatText.value = Cns.deleteName(msg);
+    const inject = () => {
+        let chatText = document.querySelector('#_chatText');
+        let update = function () {
+            let msg = chatText.value;
+            if (!msg || Cns.isDeleted(msg)) {
+                return false;
+            }
+
+            chatText.value = Cns.deleteName(msg);
+        };
+        chatText.addEventListener('blur', update);
+        chatText.addEventListener('click', update);
     };
-    chatText.addEventListener('blur', update);
-    chatText.addEventListener('click', update);
 })();
